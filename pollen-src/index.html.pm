3598a94a95ffa9f9a30608dffc1d8a659c33d9d7
Thomas Seng Hin Mak <makzan@42games.net>
2015-05-15 07:47:24 +0000

    Moving from jekyll to pallen

---
+++ pollen-src/index.html.pm
@@ -0,0 +1,5 @@
+#lang pollen
+
+◊headline{Beginning Git}
+
+We are not trying to explain everything in Git. Instead, we focus only on how to get started using Git every day in your projects.
---
+++ pollen-src/more-on-git-add.html.pm
@@ -0,0 +1,5 @@
+To add deleted file.
+
+git add . --all
+
+Why you want to add the deleted files into a commit? Because git knows if the file is just a rename or a deletion. It makes your history more accurate.
